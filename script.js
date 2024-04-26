f3d1254ccee5ffd0d9b98762ecba5c814d494649
emirgnr <emirgun.1@gmail.com>
2024-04-26 21:13:39 +0300

    v3

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ var haftaSayisi = 4;
 for (var i = 1; i <= haftaSayisi; i++) {
     // Col elementi oluştur
     var colDiv = document.createElement('div');
-    colDiv.classList.add('col');
+    colDiv.classList.add('col-12');
+    colDiv.classList.add('col-md-6');
+    colDiv.classList.add('col-lg-3');
 
     // Hafta başlığını oluştur
     var haftaBaslik = document.createElement('h5');
@@ -88,6 +90,8 @@ for (var i = 1; i <= haftaSayisi; i++) {
     // Col elementi oluştur
     var colDiv = document.createElement('div');
     colDiv.classList.add('col');
+    colDiv.classList.add('col-sm-6');
+    colDiv.classList.add('col-lg-3');
 
     // Tablo oluştur
     var table = document.createElement('table');
@@ -136,27 +140,27 @@ function hesapla() {
     var genelUcret = parseFloat(document.getElementById("genel-ucret").value) || 0;
     var toplamSaat = 0;
     var toplamMaas = 0;
-  
+
     var toplamSaatlar = {};
     var toplamMaaslar = {};
-  
+
     for (var i = 1; i <= 4; i++) {
-      var saatToplam = 0;
-      var maasToplam = 0;
-  
-      for (var j = 1; j <= 7; j++) {
-        var cs = parseFloat(document.getElementById("h" + i + "-cs-" + j).value) || 0;
-        var u = parseFloat(document.getElementById("h" + i + "-u-" + j).value) || genelUcret;
-        
-        saatToplam += cs;
-        maasToplam += cs * u;
-      }
-  
-      toplamSaat += saatToplam;
-      toplamMaas += maasToplam;
-  
-      toplamSaatlar["h" + i] = saatToplam;
-      toplamMaaslar["h" + i] = maasToplam;
+        var saatToplam = 0;
+        var maasToplam = 0;
+
+        for (var j = 1; j <= 7; j++) {
+            var cs = parseFloat(document.getElementById("h" + i + "-cs-" + j).value) || 0;
+            var u = parseFloat(document.getElementById("h" + i + "-u-" + j).value) || genelUcret;
+
+            saatToplam += cs;
+            maasToplam += cs * u;
+        }
+
+        toplamSaat += saatToplam;
+        toplamMaas += maasToplam;
+
+        toplamSaatlar["h" + i] = saatToplam;
+        toplamMaaslar["h" + i] = maasToplam;
     }
 
     document.getElementById("sh1").innerHTML = toplamSaatlar["h1"].toFixed(2);
@@ -164,18 +168,28 @@ function hesapla() {
     document.getElementById("sh3").innerHTML = toplamSaatlar["h3"].toFixed(2);
     document.getElementById("sh4").innerHTML = toplamSaatlar["h4"].toFixed(2);
 
-    document.getElementById("mh1").innerHTML = toplamMaaslar["h1"]+ "₺";
-    document.getElementById("mh2").innerHTML = toplamMaaslar["h2"]+ "₺";
-    document.getElementById("mh3").innerHTML = toplamMaaslar["h3"]+ "₺";
-    document.getElementById("mh4").innerHTML = toplamMaaslar["h4"]+ "₺";
+    document.getElementById("mh1").innerHTML = toplamMaaslar["h1"] + "₺";
+    document.getElementById("mh2").innerHTML = toplamMaaslar["h2"] + "₺";
+    document.getElementById("mh3").innerHTML = toplamMaaslar["h3"] + "₺";
+    document.getElementById("mh4").innerHTML = toplamMaaslar["h4"] + "₺";
 
     document.getElementById("topSaat").innerHTML = toplamSaat.toFixed(2);
+
+    saatDakikayaCevir(toplamSaat);
+
     document.getElementById("topMaas").innerHTML = toplamMaas + "₺";;
 
-  
- //   console.log("Toplam Saat:", toplamSaat);
- //   console.log("Toplam Maaş:", toplamMaas);
- //   console.log("Toplam Saatlar:", toplamSaatlar);
- //   console.log("Toplam Maaşlar:", toplamMaaslar);
-  }
+}
+
+function saatDakikayaCevir(tSaat) {
+
+    var saat = parseFloat(tSaat);
+
+    var tamSaat = Math.floor(saat);
+    var dakika = Math.round((saat - tamSaat) * 60);
+
+    var tSaatOut = tamSaat + " sa " + dakika + " dk";
+
+    document.getElementById("saatDakika").innerHTML = tSaatOut;
+}
 
